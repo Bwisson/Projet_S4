50acf4f44654e28fc5de0ad03c8d59605fa74374
nat-wd <nathan.roi@disroot.org>
2024-01-29 18:32:52 +0100

    test connection db + axios in react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './css/App.scss';
 import Button from './components/Button';
+import React from "react";
+import axios from 'axios';
+
+function getListUser(){
+    axios.get('./php/show/showAllUsers.php')
+        .then(response => console.log(data.response))
+}
 
 function App() {
   return (
     <div className="App">
+        {getListUser()}
         <Button text={"stylé ça mère"}></Button>
     </div>
   );
