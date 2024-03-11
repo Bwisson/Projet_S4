b0e0226be626376d69cf4e2fe844761502cd383d
nat-wd <nathan.roi@disroot.org>
2024-03-11 18:38:42 +0100

    ajout de crud

---
+++ php/crud/atelier.crud.php
@@ -0,0 +1,23 @@
+<?php
+include("function_rs_to_table.php");
+
+function createAtelier($conn, $id, $nom, $type) {
+    $sql = "INSERT INTO `Atelier` (`id`, `nom`, `type`) VALUES ('$id', '$nom', '$type')";
+    $res = mysqli_query($conn, $sql);
+    return $res;
+}
+function deleteAtelier($conn, $id) {
+    $sql = "DELETE FROM `Atelier` WHERE `id`=$id";
+    $res = mysqli_query($conn, $sql);
+    return $res;
+}
+function updateAtelier($conn, $id,$id, $nom, $type) {
+    $sql = "UPDATE `Atelier` SET `id`='$id', `nom`='$nom', `type`='$type' WHERE `id` = $id";
+    $res = mysqli_query($conn, $sql);
+    return $res;
+}
+function listAtelier($conn) {
+    $sql = "SELECT * FROM `Atelier`"; $res = mysqli_query($conn, $sql);
+    $rs = rs_to_table($res);
+    return $rs;
+}
---
+++ php/crud/modele.crud.php
@@ -0,0 +1,26 @@
+<?php
+include("function_rs_to_table.php");
+
+function createModele($conn, $id, $nom, $prenom, $genre, $tarif_horaire) {
+    $sql = "INSERT INTO `Modele` (`id`, `nom`, `prenom`, `genre`, `tarif_horaire`) VALUES ('$id', '$nom', '$prenom', '$genre', '$tarif_horaire')";
+    $res = mysqli_query($conn, $sql);
+    return $res;
+}
+
+
+function deleteModele($conn, $id) {
+    $sql = "DELETE FROM `Modele` WHERE `id`=$id";
+    $res = mysqli_query($conn, $sql);
+    return $res;
+}
+
+function updateModele($conn, $id,$id, $nom, $prenom, $genre, $tarif_horaire) {
+    $sql = "UPDATE `Modele` SET `id`='$id', `nom`='$nom', `prenom`='$prenom', `genre`='$genre', `tarif_horaire`='$tarif_horaire' WHERE `id` = $id";
+    $res = mysqli_query($conn, $sql);
+    return $res;
+}
+function listModele($conn) {
+    $sql = "SELECT * FROM `Modele`"; $res = mysqli_query($conn, $sql);
+    $rs = rs_to_table($res);
+    return $rs;
+}
