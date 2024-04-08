7c9d510b8a17c2f593ff112090862c66c4ec75a7
F1Cfghh <150327346+F1Cfghh@users.noreply.github.com>
2024-04-08 14:47:30 +0200

    Create ViewArticle.js

---
+++ react/ViewArticle.js
@@ -0,0 +1,67 @@
+/* Librairy imports */
+import React, { useState, useEffect } from "react"
+import axios from 'axios'
+
+/* components imports */
+import Button from '../Button'
+import PopUpUser from "./PopUpUser"
+
+/* css imports */
+import '../../css/cssViewsAdmin/ViewUsers.scss'
+import '../../css/cssViewsAdmin/tableAdmin.scss'
+
+
+function ViewArticle() {
+  const [article, setArticle]=useState(null)
+
+  useEffect(() => {
+    function getArticle() {
+    axios.get(".php/list/getArticle.php)
+              .then(response => {setArticle(respose.data)})
+              }
+    getArticle();
+  }, )
+}
+
+/*function Image() {
+  let image_product = null
+  if(product != null){
+    image_product = product.map(prod =>
+      <span>{prod.i}*/
+
+function Title() {
+  let title_article = null
+  if(article != null){
+    title_article = article.map(art =>
+      <span>{art.nom}</span>
+    );
+  }
+  return title_article
+}
+
+  /*function Desc() {
+  let desc_article = null
+  if(article != null){
+    desc_article = article.map(art =>
+      <p>{art.desc}</p>
+    );
+  }*/
+    
+return (
+  <div className="ViewProduct">
+    <span id="image">
+      //<ImageProduct />
+    </span>
+    <span id="title">
+      <titleArticle />
+    </span>
+    <span id="desc">
+      //<descArticle />
+    </span>
+  </div>
+
+  <div className="productCalendar">
+    //<Calendar />
+  </div>
+  )
+}
