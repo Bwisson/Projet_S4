ac621734441e3875df8daae5cab688cb7fd6fac3
F1Cfghh <150327346+F1Cfghh@users.noreply.github.com>
2024-04-10 08:21:48 +0200

    Update article.crud.php
    Ajout de la fonction select

--- php/crud/article.crud.php
+++ php/crud/article.crud.php
@@ -17,6 +17,11 @@ function updateArticle($conn, $id, $code_barre, $nom, $categorie, $couleur, $tai
     $res = mysqli_query($conn, $sql);
     return $res;
 }
+function selectArticle($conn, $id){
+    $sql = "SELECT * FROM `Article` WHERE id=$id";
+    $res = mysqli_query($conn, $sql);
+    return $res;
+}
 function listArticle($conn) {
     $sql = "SELECT * FROM `Article`";
     $res = mysqli_query($conn, $sql);
