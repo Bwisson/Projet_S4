13cb84faec9b7b1df7012b91dfc482b1a0a61cbc
nat-wd <nathan.roi@disroot.org>
2024-04-23 15:18:57 +0200

    feat : sidebar fixe

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,34 +18,38 @@ function NavBar({ admin, setDeconnection }) {
 
     return (
         <div className="NavBar">
-            <nav>
-                <div id="btnAndMenu">
-                    <Button id={"btnProfil"} text={"Profil"} onSmash={showProfilMenu} bgColor={"#2882ff"}></Button>
-                    {profilMenuVisible &&
-                        <div className={"profilMenu"}>
-                            <Link to={"info7/"}>Mon compte</Link>
-                            <Disconnection setDeconnection={setDeconnection}/>
-                        </div>
-                    }
-                </div>
+            <div className="contentNav">
 
-                <ul>
-                    <li><Link to={"info7/ViewArticles/Chevalets"}>CHEVALETS</Link></li>
-                    <li><Link to={"info7/ViewArticles/Peinture"}>PEINTURE</Link></li>
-                    <li><Link to={"info7/ViewArticles/Modeles"}>MODÈLES</Link></li>
-                    <li><Link to={"info7/ViewArticles/Ateliers"}>ATELIERS</Link></li>
-                </ul>
-                {admin ?
-                    <>
-                        <div id="divider"></div>
-                        <ul id={"adminLink"}>
-                            <li><Link to={"info7/AdminViewObjects"}>OBJETS</Link></li> {/* TODO : rajouter info7/ avant de push */}
-                            <li><Link to={"info7/AdminViewUsers"}>UTILISATEURS</Link></li>
-                        </ul>
-                    </>
-                    : null}
-                <li><Link to={"info7/Calendar"}>Calendrier</Link></li>
-            </nav>
+                <nav>
+                    <div id="btnAndMenu">
+                        <Button id={"btnProfil"} text={"Profil"} onSmash={showProfilMenu} bgColor={"#2882ff"}></Button>
+                        {profilMenuVisible &&
+                            <div className={"profilMenu"}>
+                                <Link to={"info7/"}>Mon compte</Link>
+                                <Disconnection setDeconnection={setDeconnection}/>
+                            </div>
+                        }
+                    </div>
+
+                    <ul>
+                        <li><Link to={"info7/ViewArticles/Chevalets"}>CHEVALETS</Link></li>
+                        <li><Link to={"info7/ViewArticles/Peinture"}>PEINTURE</Link></li>
+                        <li><Link to={"info7/ViewArticles/Modeles"}>MODÈLES</Link></li>
+                        <li><Link to={"info7/ViewArticles/Ateliers"}>ATELIERS</Link></li>
+                    </ul>
+                    {admin ?
+                        <>
+                            <div id="divider"></div>
+                            <ul id={"adminLink"}>
+                                <li><Link to={"info7/AdminViewObjects"}>OBJETS</Link></li>
+                                {/* TODO : rajouter info7/ avant de push */}
+                                <li><Link to={"info7/AdminViewUsers"}>UTILISATEURS</Link></li>
+                            </ul>
+                        </>
+                        : null}
+                    <li><Link to={"info7/Calendar"}>Calendrier</Link></li>
+                </nav>
+            </div>
         </div>
     )
 }
