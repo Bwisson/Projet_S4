06d4bdd1cf5bd90183b89c7483a4dd1009735351
clinb <39760867+clinb@users.noreply.github.com>
2024-05-13 19:21:51 +0200

    ok la c'est bon

--- php/create/createAnnulationModele.php
+++ php/create/createAnnulationModele.php
@@ -9,11 +9,13 @@ if (isset($_POST['id_resa'])) {
     $id_resa = htmlspecialchars($_POST['id_resa']);
     $listAnnulations = listAnnulationModele($conn);
     $id_resa_values = array_column($listAnnulations, 'id_resa');
-    $res = createAnnulationModele($conn, $id_resa);
-    if ($res) {
-        echo json_encode(true);
-    } else {
-        echo json_encode(false);
+    if (!in_array($id_resa, $id_resa_values, $strict = true)) {
+        $res = createAnnulationModele($conn, $id_resa);
+        if ($res) {
+            echo json_encode(true);
+        } else {
+            echo json_encode(false);
+        }
     }
 } else {
     echo json_encode("$_POST : non défini")
