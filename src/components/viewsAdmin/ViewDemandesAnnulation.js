df20ef33dc2b4a9dbe93c2f553277873dd4c0ed2
nat-wd <nathan.roi@disroot.org>
2024-05-15 09:27:41 +0200

    style : page demandes d'annulations

--- src/components/viewsAdmin/ViewDemandesAnnulation.js
+++ src/components/viewsAdmin/ViewDemandesAnnulation.js
@@ -1,4 +1,4 @@
-import '../../css/cssViewsAdmin/ViewUsers.scss'
+import '../../css/cssViewsAdmin/ViewDemandesAnnulation.scss'
 import React, { useState, useEffect } from "react"
 import axios from 'axios'
 import Button from '../Button.js'
